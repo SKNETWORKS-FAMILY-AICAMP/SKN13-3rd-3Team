0630220db99d2e2a0e4ba816c29ce0df9aed2456
JI0617 <effort1234@naver.com>
2025-08-29 12:39:37 +0900

    [수정] HF keys

--- rag/main.py
+++ rag/main.py
@@ -33,7 +33,7 @@ def get_chatbot():
 
     # 모델 및 벡터스토어 불러오기
     # index.faiss 다운로드
-    faiss_path = hf_hub_download(repo_id="username/oliveyoung-faiss", filename="index.faiss")
+    faiss_path = hf_hub_download(repo_id="user5810830/faiss_oliveyoung_reviews", filename="index.faiss")
 
     embedding_model = HuggingFaceEmbeddings(model_name="jhgan/ko-sbert-nli")
     vector_db = FAISS.load_local(faiss_path, embedding_model)
