cc414a1d32b30ba36f9a1305c50a481d2f6ef28a
JI0617 <effort1234@naver.com>
2025-08-29 12:47:40 +0900

    [수정] HF KEY

--- rag/main.py
+++ rag/main.py
@@ -10,7 +10,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from huggingface_hub import hf_hub_download
-
+import os
 
 
 # 환경변수 로드
@@ -33,7 +33,7 @@ def get_chatbot():
 
     # 모델 및 벡터스토어 불러오기
     # index.faiss 다운로드
-    faiss_path = hf_hub_download(repo_id="user5810830/faiss_oliveyoung_reviews", filename="index.faiss")
+    faiss_path = hf_hub_download(repo_id="user5810830/faiss_oliveyoung_reviews", filename="index.faiss", token=os.getenv("HUGGINGFACE_API_KEY"))
 
     embedding_model = HuggingFaceEmbeddings(model_name="jhgan/ko-sbert-nli")
     vector_db = FAISS.load_local(faiss_path, embedding_model)
